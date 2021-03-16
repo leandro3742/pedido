3d2746a8625917fdabea175b3bcf58540f58c274
Leandro Marrero <leandro.marrero@outlook.com>
2021-03-16 00:40:38 -0300

    cambios

--- src/Componentes/Guardar_mesa.js
+++ src/Componentes/Guardar_mesa.js
@@ -3,13 +3,12 @@ import './Styles/Guardar_mesa.css';
 
 
 const Guardar_mesa = (props) => {
-    const [state, setState] = useState("none");
+    const [state, setState] = useState("Elija una mesa");
 
     function HandleChange(event){
         console.log(event.target.value);
         setState(event.target.value);
         guardar_mesa(event.target.value);
-
     }
 
     function mandar_mensaje(){
@@ -23,12 +22,13 @@ const Guardar_mesa = (props) => {
             <form className="formulario_mesa" >
                 <span className="label">Elije el numero de tu mesa: </span> 
                 <select className="opciones" value={state} onChange={HandleChange}>
+                    <option value="elegir una mesa">Elija una mesa</option>
                     <option value="1">Mesa 1</option>
                     <option value="2">Mesa 2</option>
                     <option value="3">Mesa 3</option>
                     <option value="4">Mesa 4</option>
                 </select>
-                <button className="boton" onClick={ () => mandar_mensaje()}>Seguir comprando</button>
+                <button className="boton_form" onClick={ () => mandar_mensaje()}>Seguir comprando</button>
             </form>
         </div>
     );
